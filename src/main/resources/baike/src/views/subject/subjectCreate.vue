3b8716c18266f0c317c10c995f6e20da2783c028
Tshirt-hfk <fanglei212@outlook.com>
2019-07-12 23:55:38 +0800

    专题创建页面添加文件类型输入

--- src/main/resources/baike/src/views/subject/subjectCreate.vue
+++ src/main/resources/baike/src/views/subject/subjectCreate.vue
@@ -6,11 +6,7 @@
         <el-input v-model="form.name"></el-input>
       </el-form-item>
       <el-form-item label="专题领域">
-        <el-select
-          v-model="form.field"
-          filterable
-          placeholder="请选择专题领域"
-        >
+        <el-select v-model="form.field" filterable placeholder="请选择专题领域">
           <el-option v-for="field in fields" :key="field" :label="field" :value="field"></el-option>
         </el-select>
       </el-form-item>
@@ -25,6 +21,9 @@
       </el-form-item>
       <el-form-item label="专题目标">
         <el-input type="textarea" rows="4" v-model="form.goal" maxlength="100" show-word-limit></el-input>
+      </el-form-item>
+      <el-form-item label="相关文件">
+        <input type="file" accept="text/plain" @change="getFile" />
       </el-form-item>
       <el-form-item label="公开">
         <el-switch v-model="form.isPublic"></el-switch>
@@ -63,18 +62,25 @@ export default {
         field: "",
         isPublic: false,
         introduction: "",
-        goal: ""
+        goal: "",
+        filedata: ""
       },
       fields: ["科学"]
     };
   },
   methods: {
+    getFile: function(event) {
+      // 获取input里的文件
+      var file=event.target.files[0];
+      var reader = new FileReader();
+      reader.readAsText(file, "UTF-8")
+      reader.onload = (event) => {
+        this.form.filedata = event.target.result;
+      }
+    },
     submit() {
       this.$axios
-        .post(
-          "http://localhost:8081/api/subjectMaker/createSubject",
-          this.form
-        )
+        .post("http://localhost:8081/api/subjectMaker/createSubject", this.form)
         .then(res => {
           if (res.data) {
             this.$message({
@@ -103,6 +109,12 @@ export default {
 </script>
 
 <style>
+.file {
+  outline: none;
+  background-color: transparent;
+  filter: alpha(opacity=0);
+  opacity: 0;
+}
 .title {
   margin: 20px auto;
 }
